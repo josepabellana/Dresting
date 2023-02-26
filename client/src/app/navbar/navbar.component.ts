0f7c54a3d37bf9c1de8317e3f241da564df3d803
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-26 21:24:57 +0100

    x

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject,HostListener } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 @Component({
@@ -11,4 +11,21 @@ export class NavbarComponent {
   // Inject the authentication service into your component through the constructor
   constructor(@Inject(DOCUMENT) public document: Document,public auth: AuthService) {}
 
+  dropShowProduct() {
+    document.getElementById("dropdown__product")?.classList.toggle("show");
+  }
+  
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: any) {
+    if (!event?.target?.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 }
