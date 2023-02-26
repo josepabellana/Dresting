abe5bf98ffd7de45193ca55d9b75b71f0ca59cb5
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-26 18:51:57 +0100

    x

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  // Inject the authentication service into your component through the constructor
+  constructor(@Inject(DOCUMENT) public document: Document,public auth: AuthService) {}
 
 }
