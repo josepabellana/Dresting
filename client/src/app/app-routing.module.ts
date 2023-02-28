0d97fd54f2020cbe26da127213dfb3a91308a603
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-28 23:07:19 +0100

    x

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddDressComponent } from './add-dress/add-dress.component';
 import { DressListComponent } from './dress-list/dress-list.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'home', component: DressListComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'seller', component: AddDressComponent},
   { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
